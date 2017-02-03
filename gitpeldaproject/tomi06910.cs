090ed7f24edda0e0804576019329b09802e1ff0a
tomi06910 <tomi06910@gmail.com>
2017-02-03 13:36:32 +0100

    4.csillag

--- gitpeldaproject/tomi06910.cs
+++ gitpeldaproject/tomi06910.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace gitpeldaproject
+namespace tomi06910
 {
-    class tomi06910
+  public static  class tomi06910
     {
+        public static void  Kiir()
+        {
+            Console.WriteLine("********tomi06910**************");
+
+
+        } 
+
+
+
     }
 }
