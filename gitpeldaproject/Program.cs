23ee856b68ce566a9f4392d2333479f340768198
AlexaTorok <toroknico97@gmail.com>
2017-02-03 15:43:54 +0100

    ttt

--- gitpeldaproject/Program.cs
+++ gitpeldaproject/Program.cs
@@ -19,7 +19,6 @@ namespace gitpeldaproject
             KristofKovacs1.KristofKovacs1.Kiír();        
             GombasBence.GombasBence.Kiir();
             DekanySzabolcs.DekanySzabolcs.Kiir();
-
             a_sajat_nevem.A_Te_Neved.Kiir();
             KristofKovacs1.KristofKovacs1.Kiír();
             vataligergo.vataligergo.kiir();          
@@ -34,8 +33,6 @@ namespace gitpeldaproject
             susagabo.susagabo.kiir();
             skyland0629.skyland0629.kiir();
             Console.ReadKey();
-            
-            
         }
     }
 }
