c30a364698dcfa24e2321fce9253747f23e32de3
MartonLabancz <labancz.marton@gmail.com>
2017-02-03 13:26:52 +0100

    issue_25 done

---
+++ gitpeldaproject/MartonLabancz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SquatPepe
+{
+    public static class SquatPepe
+    {
+        public static void kiir()
+        {
+            Console.WriteLine("SquatPepe");
+
+        }
+
+    }
+}
