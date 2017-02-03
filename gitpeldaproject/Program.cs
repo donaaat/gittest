13a2c4c5db9dbe2f783a97e117b82578d8b8f2be
markkoltai <markkoltai@gmail.com>
2017-02-03 13:32:08 +0100

    Issue 12 fixered (Szeretem a sajtot)

--- gitpeldaproject/Program.cs
+++ gitpeldaproject/Program.cs
@@ -9,6 +9,7 @@ namespace gitpeldaproject
     {
         static void Main(string[] args)
         {
+            markkoltai.markkoltai.kiir();
         }
     }
 }
---
+++ gitpeldaproject/markkoltai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace markkoltai
+{
+    public static class markkoltai
+    {
+        public static void kiir()
+        {
+            Console.WriteLine("markkoltai");
+        }
+    }
+}
