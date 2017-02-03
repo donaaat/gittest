30ee15936bc55b2b5534fb3b3c5181ab6665f743
BeatrixSzekeres <rixi1125@gmail.com>
2017-02-03 13:30:27 +0100

    issue_8 done

--- gitpeldaproject/rixi96.cs
+++ gitpeldaproject/rixi96.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace gitpeldaproject
+namespace rixi96
 {
-    class Program
+    public static class rixi96
     {
-        static void Main(string[] args)
+        public static void Kiir()
         {
-            rixi96.rixi96.Kiir();
+            Console.WriteLine("rixi96");
         }
     }
 }
